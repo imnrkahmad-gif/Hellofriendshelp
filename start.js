2efa1478a83ccd3df87b692fe7fde79a34f67739
HelloFriendshelp <imnrkahmad@gmail.com>
2026-08-26 15:05:17 +0530

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,20 @@
-require("./server.js");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const Module = require("module");
+
+const sourcePath = path.join(__dirname, "server.js");
+let source = fs.readFileSync(sourcePath, "utf8");
+
+source = source.replace(
+  /\$\{Object\.keys\(CONTENT\)\.map\(s=>.*?\)\.join\("")\}/,
+  '${Object.keys(CONTENT).map(s => "<option>" + s + "</option>").join("")}'
+);
+
+const fixedPath = path.join(os.tmpdir(), "nursestudy-server-fixed.js");
+fs.writeFileSync(fixedPath, source, "utf8");
+
+const mod = new Module(fixedPath, module);
+mod.filename = fixedPath;
+mod.paths = Module._nodeModulePaths(path.dirname(fixedPath));
+mod._compile(source, fixedPath);
